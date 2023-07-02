3f229cfb3509cd126c7f28c04db0067df909cf45
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-02 14:23:30 -0400

    mobile completed

--- 9-FaqAccordionCard/index.js
+++ 9-FaqAccordionCard/index.js
@@ -1,33 +1,29 @@
 const titles = Array.from(document.getElementsByClassName("title"));
-const description = Array.from(document.getElementsByClassName("description"));
-const arrowIcon = Array.from(document.querySelectorAll(".faqs img"));
-let presionado = null;
+const descriptions = Array.from(document.getElementsByClassName("description"));
+const arrowIcons = Array.from(document.querySelectorAll(".faqs img"));
+let clickedTitle = null;
 
-description.forEach(element => {
-    element.style.display = "none";
-});
-
-function normmalPosition(position) {
-    titles[position].style.fontWeight = "normal";
-    description[position].style.display = "none";
-    arrowIcon[position].style.transform = "rotate(0deg)";
+function hideAllDescriptions() {
+    titles.forEach((title, index) => {
+        title.style.fontWeight = "normal";
+        descriptions[index].style.display = "none";
+        arrowIcons[index].style.transform = "rotate(0deg)";
+    });
 }
-
-titles.forEach(title => {
+titles.forEach((title) => {
     title.addEventListener("click", function () {
         const position = titles.indexOf(this);
-        if (presionado == this) {
-            normmalPosition(position);
-            presionado = null;
+        if (clickedTitle === this) {
+            hideAllDescriptions();
+            clickedTitle = null;
             return;
         }
-        presionado = this;
-        for (let index = 0; index < description.length; index++) {
-            normmalPosition(index);
-        }
+        clickedTitle = this;
+        hideAllDescriptions();
 
         titles[position].style.fontWeight = "bold";
-        description[position].style.display = "block";
-        arrowIcon[position].style.transform = "rotate(180deg)";
+        descriptions[position].style.display = "block";
+        arrowIcons[position].style.transform = "rotate(180deg)";
     });
 });
+hideAllDescriptions();
